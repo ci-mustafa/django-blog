97508113c04f2d702368b0de3b51673007eb33bf
ci-mustafa akbari <akbarimustafa64@gmail.com>
2024-09-11 19:14:25 +0000

    feat: add two more fields in post model

--- blog/models.py
+++ blog/models.py
@@ -12,7 +12,9 @@ class Post(models.Model):
     title = models.CharField(max_length=200, unique=True)
     slug = models.SlugField(max_length=200, unique=True)
     content = models.TextField()
-    created_on = models.DateField(auto_now_add=True)
+    created_on = models.DateTimeField(auto_now_add=True)
     status = models.IntegerField(choices=STATUS_CHOICES, default=DRAFT)
+    excerpt = models.TextField(null=True, blank=True)
+    updated_on = models.DateTimeField(auto_now=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="blog_posts")
 
