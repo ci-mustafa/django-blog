438a19250a3a256426136e429bc7c857a019e4fd
ci-mustafa akbari <akbarimustafa64@gmail.com>
2024-09-12 14:54:42 +0000

    feat: register comment model to admin site and migrate it

--- blog/admin.py
+++ blog/admin.py
@@ -1,8 +1,14 @@
 from django.contrib import admin
-from .models import Post
+from .models import Post, Comment
 
 # Register your models here.
 
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
     fields = ["title", "slug", "content", "excerpt", "status", "author"]
+
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    fields = ["post", "author", "body", "approved"]
