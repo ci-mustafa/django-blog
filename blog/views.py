e53a127ac23af4ec2d701c44631e2fef858e58c8
ci-mustafa akbari <akbarimustafa64@gmail.com>
2024-09-14 08:27:03 +0000

    feat: declare custom context object name for PostList view

--- blog/views.py
+++ blog/views.py
@@ -2,11 +2,14 @@ from django.shortcuts import render
 from django.views import generic
 from . import models
 
-# Create your views here.
+# Create class based generic view to list all posts from database
 class PostList(generic.ListView):
+    # queryset to fetch all posts
     queryset = models.Post.objects.all()
+    # template name 
     template_name = "post_list.html"
-
+    # declare a custom context object name
+    context_object_name = "posts"
     
 
 
