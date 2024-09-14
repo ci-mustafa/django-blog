94a571220c9d03b50e8a2191122ed427b7912105
ci-mustafa akbari <akbarimustafa64@gmail.com>
2024-09-14 13:54:11 +0000

    feat: Load sample data for testing using fixtures

--- blog/admin.py
+++ blog/admin.py
@@ -1,11 +1,16 @@
 from django.contrib import admin
+from django_summernote.admin import SummernoteModelAdmin
 from .models import Post, Comment
 
 # Register your models here.
 
 @admin.register(Post)
-class PostAdmin(admin.ModelAdmin):
+class PostAdmin(SummernoteModelAdmin):
     list_display = ["title", "slug", "content", "excerpt", "status", "author"]
+    search_fields = ['title']
+    list_filter = ('status','created_on')
+    prepopulated_fields = {'slug': ('title',)}
+    summernote_fields = ("content")
     
     
 
