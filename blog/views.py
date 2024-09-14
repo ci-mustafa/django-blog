5d99b417b4a317163f4c4ad2cc5ac2cbab74ba48
ci-mustafa akbari <akbarimustafa64@gmail.com>
2024-09-14 08:15:12 +0000

    feat: generic post view

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.views import generic
+from . import models
 
 # Create your views here.
+class PostList(generic.ListView):
+    queryset = models.Post.objects.all()
+    template_name = "post_list.html"
 
-def index(request):
-    return HttpResponse("Hello, Blog!")
+    
+
+
