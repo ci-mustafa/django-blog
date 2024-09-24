1ee2aec32aa63239410227e511fb9cbb287a5952
Mustafa Akbari <akbarimustafa64@gmail.com>
2024-09-24 16:46:41 +0000

    feat: add delete ability to each comments

--- blog/urls.py
+++ blog/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from .views import PostList, post_detail, comment_edit
+from .views import PostList, post_detail, comment_edit, comment_delete
 
 urlpatterns = [
     path("", PostList.as_view(), name="home"),
     path('<slug:slug>/', post_detail, name='post_detail'),
     path('<slug:slug>/edit_comment/<int:comment_id>',
          comment_edit, name='comment_edit'),
+    path('<slug:slug>/delete_comment/<int:comment_id>',
+         comment_delete, name='comment_delete'),
 ]
