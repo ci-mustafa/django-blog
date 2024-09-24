ca967641c71d705f2fa840c5d6204caae9b458b1
Mustafa Akbari <akbarimustafa64@gmail.com>
2024-09-24 16:24:00 +0000

    feat: add update ability to each comment

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import PostList, post_detail
+from .views import PostList, post_detail, comment_edit
 
 urlpatterns = [
     path("", PostList.as_view(), name="home"),
     path('<slug:slug>/', post_detail, name='post_detail'),
+    path('<slug:slug>/edit_comment/<int:comment_id>',
+         comment_edit, name='comment_edit'),
 ]
