567252cecab379a1e0aedd059295dbdab520ed3c
Mustafa Akbari <akbarimustafa64@gmail.com>
2024-09-25 14:46:52 +0000

    feat: add profile image for about

--- about/models.py
+++ about/models.py
@@ -1,9 +1,11 @@
 from django.db import models
+from cloudinary.models import CloudinaryField
 
 # Create your models here.
 
 
 class About(models.Model):
+    profile_image = CloudinaryField('image', default='placeholder')
     title = models.CharField(max_length=100)
     content = models.TextField()
     updated_on = models.DateTimeField(auto_now=True)
